62fb2ef53a8845f16103a8378a93cce2c4c8ebe1
sabharanikumar <119396551+sabharanikumar@users.noreply.github.com>
2024-06-28 15:11:51 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,8 +3,6 @@ let labels = [];
 let usedMemoryData = [];
 let cpuUsageData = [];
 let domNodeCountData = [];
-
-// Create a chart instance for memory usage
 const ctxMemory = document.getElementById('memoryChart').getContext('2d');
 const memoryChart = new Chart(ctxMemory, {
   type: 'line',
@@ -51,8 +49,6 @@ const memoryChart = new Chart(ctxMemory, {
     }
   }
 });
-
-// Create a chart instance for DOM node count
 const ctxDomNodeCount = document.getElementById('domNodeCountChart').getContext('2d');
 const domNodeCountChart = new Chart(ctxDomNodeCount, {
   type: 'line',
@@ -83,8 +79,6 @@ const domNodeCountChart = new Chart(ctxDomNodeCount, {
     }
   }
 });
-
-// Create a chart instance for CPU usage
 const ctxCPU = document.getElementById('cpuChart').getContext('2d');
 const cpuChart = new Chart(ctxCPU, {
   type: 'line',
@@ -135,43 +129,22 @@ const cpuChart = new Chart(ctxCPU, {
 function updateCharts(memoryInfo, cpuUsage, domNodeCount) {
   // Convert bytes to megabytes
   const usedMemoryMB = memoryInfo.usedMemory / 1048576;
-
-  // Add current time to labels array
   labels.push(new Date().toLocaleTimeString());
-
-  // Add used memory to usedMemoryData array
   usedMemoryData.push(usedMemoryMB);
-
-  // Add CPU usage to cpuUsageData array (multiplied by 100 to represent percentage)
   cpuUsageData.push(cpuUsage * 100);
-
-  // Add DOM node count to domNodeCountData array
   domNodeCountData.push(domNodeCount);
-
-  // Update memory chart data
   memoryChart.data.datasets[0].data.push(usedMemoryMB);
   memoryChart.update();
-
-  // Update CPU chart data
   cpuChart.data.datasets[0].data.push(cpuUsage * 100); // Multiply by 100 for percentage
   cpuChart.update();
-
-  // Update DOM node count chart data
   domNodeCountChart.data.datasets[0].data.push(domNodeCount);
   domNodeCountChart.update();
-
-  // Display total heap memory and max heap memory
   document.getElementById('totalMemory').innerText = 'Total Heap Memory: ' + (memoryInfo.totalMemory / 1048576).toFixed(2) + ' MB';
   document.getElementById('maxHeap').innerText = 'Max Heap Memory: ' + (memoryInfo.heaplimit / 1048576).toFixed(2) + ' MB';
-
-  // Display CPU usage
   document.getElementById('cpuUsage').innerText = 'CPU Usage: ' + (cpuUsage * 100).toFixed(2) + ' %';
-
-  // Display DOM node count
   document.getElementById('domNodeCount').innerText = 'DOM Node Count: ' + domNodeCount;
 }
 
-// Function to send message to content script to get memory and CPU info
 function getInfo() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { action: "getInfo" }, function (response) {
@@ -183,6 +156,4 @@ function getInfo() {
     });
   });
 }
-
-// Send message to content script every second
 setInterval(getInfo, 1500);
